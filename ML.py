7a8364f192d2707dabbecbdf1e4e8326eae39254
Harrison Bacordo <bacordharr@myvuw.ac.nz>
2018-07-19 13:35:28 +1200

    Comments and variable refactorings

--- ML.py
+++ ML.py
@@ -1,36 +1,40 @@
 from sklearn import neural_network, neighbors, tree, svm, naive_bayes, metrics, datasets
 
 # Where pipeline will be
-iris = datasets.load_iris()
+# TODO add actual dataset here. iris dataset is just a placeholder
+dataset = datasets.load_iris()
 
-#
+# Analogizers
 model = neighbors.KNeighborsClassifier()
-model.fit(iris.data, iris.target)
-predictions = model.predict(iris.data)
-print(metrics.accuracy_score(predictions, iris.target))
+model.fit(dataset.data, dataset.target)
+predictions = model.predict(dataset.data)
+print(metrics.accuracy_score(predictions, dataset.target))
 
-
-model = neural_network.MLPClassifier()
-model.fit(iris.data, iris.target)
-predictions = model.predict(iris.data)
-score = metrics.accuracy_score(predictions, iris.target)
-print(score)
-
-model = tree.DecisionTreeClassifier()
-model.fit(iris.data, iris.target)
-predictions = model.predict(iris.data)
-score = metrics.accuracy_score(predictions, iris.target)
-print(score)
-
+# ALSO ANALOGIZERS
 model = svm.SVC()
-model.fit(iris.data, iris.target)
-predictions = model.predict(iris.data)
-score = metrics.accuracy_score(predictions, iris.target)
+model.fit(dataset.data, dataset.target)
+predictions = model.predict(dataset.data)
+score = metrics.accuracy_score(predictions, dataset.target)
 print(score)
 
+# Connectionists
+model = neural_network.MLPClassifier()
+model.fit(dataset.data, dataset.target)
+predictions = model.predict(dataset.data)
+score = metrics.accuracy_score(predictions, dataset.target)
+print(score)
+
+# Symbolists
+model = tree.DecisionTreeClassifier()
+model.fit(dataset.data, dataset.target)
+predictions = model.predict(dataset.data)
+score = metrics.accuracy_score(predictions, dataset.target)
+print(score)
+
+# Bayesians
 model = naive_bayes.GaussianNB()
-model.fit(iris.data, iris.target)
-predictions = model.predict(iris.data)
-score = metrics.accuracy_score(predictions, iris.target)
+model.fit(dataset.data, dataset.target)
+predictions = model.predict(dataset.data)
+score = metrics.accuracy_score(predictions, dataset.target)
 print(score)
 
